3bc2528fdc502a2ec6629f7d03c7ef896e2ac706
Ani2nem <ani2nem@gmail.com>
2023-11-05 11:41:03 -0600

    last commit, added the choose file button

--- components/cta.js
+++ components/cta.js
@@ -2,30 +2,44 @@ import React from "react";
 import Container from "./container";
 
 const Cta = () => {
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    // Use the file as needed, e.g., upload to server or process locally.
+  };
+
   return (
     <Container>
-      <div className="spill-js flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
-        <div className="flex-grow text-center lg:text-left">
+      <div className="spill-js flex items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 rounded-xl">
         <input
           type="text"
           placeholder="Your CSV file goes here"
-          className="p-3 bg-white rounded-md w-full text-black"
-          style={{ height: '67px', fontSize: '23px', fontWeight: '500'}}
+          className="flex-grow p-3 bg-white rounded-md text-black"
+          style={{ height: '67px', fontSize: '23px', fontWeight: '500' }}
         />
-        </div>
-        <div className="flex-shrink-0 w-full text-center lg:w-auto">
-          <a
-            href="https://github.com/web3templates"
-            target="_blank"
-            rel="noopener"
-            className="inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-10 lg:py-5 "
-            style={{ fontSize: '23px', fontWeight: 'bold' }}>
-            Submit
-          </a>
-        </div>
+
+        <a
+          href="https://github.com/web3templates"
+          target="_blank"
+          rel="noopener"
+          className="flex-shrink-0 p-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md"
+          style={{ height: '67px', fontSize: '23px', fontWeight: 'bold', width: '25%' }}
+        >
+          Submit
+        </a>
+
+        <label htmlFor="file-upload" className="flex-grow text-center lg:text-left p-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md">
+          <input
+            type="file"
+            id="file-upload"
+            onChange={handleFileChange}
+            className="file-input"
+            style={{ display: 'none' }}
+          />
+          <span style={{ height: '40px', fontWeight: 'bold', display: 'block' }}>Choose File</span>
+        </label>
       </div>
     </Container>
   );
-}
+};
 
 export default Cta;
